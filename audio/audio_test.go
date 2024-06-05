be027529d29d1bfff338e50c0fb54bf25da7d883
Tsepo Nkalai <nkalait@gmail.com>
2024-06-05 21:21:47 +0200

    continue add testing

--- audio/audio_test.go
+++ audio/audio_test.go
@@ -1,17 +1,17 @@
-package tests
+package audio_test
 
 import (
 	"mamela/audio"
 	"testing"
 )
 
-func PadTestSingle(t *testing.T) {
+func TestPadSingle(t *testing.T) {
 	padded := audio.Pad(0)
 	if padded != "00" {
 		t.Errorf("pad(0) = %s; want 00", padded)
 	}
 }
-func PadTestDouble(t *testing.T) {
+func TestPadDouble(t *testing.T) {
 	padded := audio.Pad(11)
 	if padded != "11" {
 		t.Errorf("pad(11) = %s; want 11", padded)
